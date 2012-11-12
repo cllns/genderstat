71fc8a51393c2ec66eb9e78096beb4d7d265145f
sean collins <sean@cllns.com>
2012-11-11 20:17:23 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,75 @@
+demasc - a text analyzer for gendered language
+======
+More specifically, calclulates the amount (in terms of percentages and relative ratios) of gendered language in a file or on a site. 
+It counts the number of words like "he","his","him", etc. and compares it to the number of words like "she", "hers", "her", etc.
+
+Why?
+----
+Because gendered language reinforces sexism.
+
+Example
+-------
+    ruby
+The ralph-waldo-emerson.txt file is a collection of his essays and kate-chopin.txt is her story The Awakening and other short stories.    
+    $ ./demasc.rb ralph-waldo-emerson.txt 
+    reading in file: ralph-waldo-emerson.txt
+    
+    total words:     77181
+    masculine words: 1798
+    feminine words:  79
+    neutral words:   1524
+    
+    masculine words: 2.33%
+    feminine words: 0.10%
+    neutral words: 1.97%
+    22.76 times as many masculine words as there are feminine words
+
+    $ ./demasc.rb kate-chopin.txt 
+    reading in file: kate-chopin.txt
+    
+    total words:     67024
+    masculine words: 1705
+    feminine words:  3125
+    neutral words:   961
+    
+    masculine words: 2.54%
+    feminine words: 4.66%
+    neutral words: 1.43%
+    1.83 times as many feminine words as there are masculine words
+
+
+    $ ./demasc.rb feministing.com
+    reading in website: http://feministing.com
+    
+    total words:     4511
+    masculine words: 4
+    feminine words:  21
+    neutral words:   15
+    
+    masculine words: 0.09%
+    feminine words: 0.47%
+    neutral words: 0.33%
+    5.25 times as many feminine words as there are masculine words
+
+
+    $ ./demasc.rb stallman.org
+    reading in website: http://stallman.org
+    
+    total words:     9440
+    masculine words: 22
+    feminine words:  2
+    neutral words:   74
+    
+    masculine words: 0.23%
+    feminine words: 0.02%
+    neutral words: 0.78%
+    11.00 times as many masculine words as there are feminine words
+    
+
+Dependencies
+------------
+- [ruby-readability](https://github.com/iterationlabs/ruby-readability) - Required for URL's
+
+Contribute
+----------
+Send a pull request or let me know of any feature requests. The wordlists could use work I'm sure.
---
+++ demasc.rb
@@ -0,0 +1,82 @@
+#! /usr/bin/ruby
+# A program to determine how "masculine" a piece of writing is.
+# Sean Collins 11/11/12
+
+require 'open-uri'
+require 'readability'
+require 'set'
+
+if ARGV.length != 1
+  abort("Usage: demasc [FILE] or demasc [URL]")
+end
+
+arg = ARGV[0]
+
+if File.exist?(arg)
+  puts "reading in file: " + arg
+  text = IO.read(arg)
+else
+  if arg[0,7] != "http://"
+    arg = "http://" + arg
+  end
+  puts "reading in website: " + arg
+  text = open(arg).read
+end
+
+
+mas_words = Set.new ["he", "him", "his", "he's", "himself", "man", "dude", "boy", "mankind"]
+fem_words = Set.new ["she", "her", "hers", "she's", "herself", "woman", "lady", "girl", "womankind"]
+neu_words = Set.new ["it", "its", "itself", "it's", "humankind", "they"]
+
+all_words = text.downcase.split(" ")
+
+num_mas_words = 0
+num_fem_words = 0
+num_neu_words = 0
+num_other_words = 0
+
+all_words.each { |word|
+  if mas_words.include?(word)
+    num_mas_words+=1
+  elsif fem_words.include?(word)
+    num_fem_words+=1
+  elsif neu_words.include?(word)
+    num_neu_words+=1
+  else
+    num_other_words+=1
+  end
+}
+
+per_mas_words = ((num_mas_words.to_f / all_words.length.to_f) * 100)
+per_fem_words = ((num_fem_words.to_f / all_words.length.to_f) * 100)
+per_neu_words = ((num_neu_words.to_f / all_words.length.to_f) * 100)
+
+# cast to float to allow for infinity
+rat_m_to_f = num_mas_words.to_f / num_fem_words
+# Yes of course I could just use the inverse of rat_m_to_f but why bother?
+rat_f_to_m = num_fem_words.to_f / num_mas_words
+
+#clean up the floats
+per_mas_words_str = sprintf('%.02f', per_mas_words) + "%"
+per_fem_words_str = sprintf('%.02f', per_fem_words) + "%"
+per_neu_words_str = sprintf('%.02f', per_neu_words) + "%"
+rat_m_to_f_str = sprintf('%.02f', rat_m_to_f) 
+rat_f_to_m_str = sprintf('%.02f', rat_f_to_m) 
+
+puts
+puts "total words:     " + all_words.length.to_s
+puts "masculine words: " + num_mas_words.to_s
+puts "feminine words:  " + num_fem_words.to_s
+puts "neutral words:   " + num_neu_words.to_s
+puts
+puts "masculine words: " + per_mas_words_str
+puts "feminine words: " + per_fem_words_str
+puts "neutral words: " + per_neu_words_str
+
+if rat_m_to_f > 1
+  puts rat_m_to_f_str + " times as many masculine words as feminine words"
+elsif rat_f_to_m > 1
+  puts rat_f_to_m_str + " times as many feminine words are masculine words"
+else
+  puts "Same number of masculine and feminine words"
+end
