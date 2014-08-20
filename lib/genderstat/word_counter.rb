af4e7634a4b4ab690d8b3fcfd1bcc70822e6f027
Sean Collins <sean@cllns.com>
2014-08-20 12:30:26 -0400

    combine word_counter and word_list (into the former)

--- lib/genderstat/word_counter.rb
+++ lib/genderstat/word_counter.rb
@@ -1,14 +1,14 @@
-require './lib/genderstat/word_list'
+require 'set'
 
 class WordCounter
   attr_reader :counter
 
   def initialize filename
-    @word_list = WordList.new filename
+    @words = Set.new YAML.load_file filename
     @counter = 0
   end
 
   def is_in_here? word_in_question
-    @counter += 1 if @word_list.contains? word_in_question
+    @counter += 1 if @words.include? word_in_question
   end
 end
