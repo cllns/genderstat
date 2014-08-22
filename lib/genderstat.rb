1348640926ff4a7027732fac9a7ed5f5798d1921
Sean Collins <sean@cllns.com>
2014-08-22 00:01:11 -0400

    move relative_frequency into calculator and clean up some printing as a percent, with 2 decimal places

--- lib/genderstat.rb
+++ lib/genderstat.rb
@@ -27,37 +27,25 @@ class Genderstat
   end
 
   def print_results
-    # calculate percentages
-    per_mas_words = @mas_words.get_percent @all_words.count
-    per_fem_words = @fem_words.get_percent @all_words.count
-    per_neu_words = @neu_words.get_percent @all_words.count
+    puts
+    puts "total words:     #{@all_words.count.to_s}"
+    puts "masculine words: #{@mas_words.counter.to_s}"
+    puts "feminine words:  #{@fem_words.counter.to_s}"
+    puts "neutral words:   #{@neu_words.counter.to_s}"
+    puts
+    puts "masculine words: #{@mas_words.get_percent @all_words.count}%"
+    puts "feminine words:  #{@fem_words.get_percent @all_words.count}%"
+    puts "neutral words:   #{@neu_words.get_percent @all_words.count}%"
 
     # cast to float to allow for infinity
-    rat_m_to_f = @mas_words.counter.to_f / @fem_words.counter
+    rat_m_to_f = @mas_words.get_relative_frequency @fem_words.counter
     # Yes of course I could just use the inverse of rat_m_to_f but why bother?
-    rat_f_to_m = @fem_words.counter.to_f / @mas_words.counter
-
-    #clean up the floats. no one cares about more than a couple decimal digits"
-    per_mas_words_str = sprintf('%.02f', per_mas_words) + "%"
-    per_fem_words_str = sprintf('%.02f', per_fem_words) + "%"
-    per_neu_words_str = sprintf('%.02f', per_neu_words) + "%"
-    rat_m_to_f_str = sprintf('%.02f', rat_m_to_f)
-    rat_f_to_m_str = sprintf('%.02f', rat_f_to_m)
-
-    puts
-    puts "total words:     " + @all_words.length.to_s
-    puts "masculine words: " + @mas_words.counter.to_s
-    puts "feminine words:  " + @fem_words.counter.to_s
-    puts "neutral words:   " + @neu_words.counter.to_s
-    puts
-    puts "masculine words: " + per_mas_words_str
-    puts "feminine words: " + per_fem_words_str
-    puts "neutral words: " + per_neu_words_str
+    rat_f_to_m = @fem_words.get_relative_frequency @mas_words.counter
 
     if rat_m_to_f > 1
-      puts rat_m_to_f_str + " times as many masculine words as feminine words"
+      puts "#{rat_m_to_f} times as many masculine words as feminine words"
     elsif rat_f_to_m > 1
-      puts rat_f_to_m_str + " times as many feminine words are masculine words"
+      puts "#{rat_f_to_m} times as many feminine words are masculine words"
     else
       puts "Same number of masculine and feminine words"
     end
--- lib/genderstat/calculator.rb
+++ lib/genderstat/calculator.rb
@@ -1,7 +1,15 @@
 module Calculator
 
   def get_percent total_word_count
-    (@counter.to_f / total_word_count.to_f) * 100
+    round (@counter.to_f / total_word_count.to_f) * 100
   end
 
+  def get_relative_frequency other_count
+    round @counter.to_f / other_count.to_f
+  end
+
+  # round all responses to two digits after the decimal point
+  def round float
+    float.round(2)
+  end
 end
